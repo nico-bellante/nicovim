55fdc7193b807ee41c7c4d0eda194c976df01f3b
Nico Bellante <nico.bellante@discordapp.com>
2022-09-27 22:01:49 +0000

    a bit more progress, got null-ls formatting working and maybe some lsp stuff and basic options

--- lua/options.lua
+++ lua/options.lua
@@ -1,1 +1,43 @@
-vim.cmd[[colorscheme tokyonight]]
+-- set colorscheme
+vim.cmd([[colorscheme tokyonight]])
+
+-- format on save
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]])
+
+local opt = vim.opt
+local g = vim.g
+
+opt.title = true
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+
+-- indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+
+-- numbers
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+
+-- disable nvim intro
+opt.shortmess:append("sI")
+
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append("<>[]hl")
+
+g.mapleader = " "
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,9 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
     return true
   end
   return false
@@ -11,17 +11,46 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- My plugins here
-  use 'folke/tokyonight.nvim' -- color scheme
-  use 'tpope/fugitive' -- git stuff
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
+return require("packer").startup(function(use)
+  use("wbthomason/packer.nvim")
+  -- my plugins here
+  -- core stuff
+
+  use("nvim-lua/plenary.nvim")
+
+  -- theme stuff
+  use("kyazdani42/nvim-web-devicons")
+  use("folke/tokyonight.nvim") -- color scheme
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    config = function()
+      require("lualine").setup()
+    end,
+  })
+
+  -- git stuff
+  use("tpope/vim-fugitive")
+
+  -- lsp stuff
+  use({
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("plugins.lspconfig")
+    end,
+  })
+
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    after = "nvim-lspconfig",
+    config = function()
+      require("plugins.null-ls")
+    end,
+  })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
