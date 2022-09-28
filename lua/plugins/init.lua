ecb3cef12f28d3c70d27d08e643d3c40708c353d
Nico Bellante <nico.bellante@discordapp.com>
2022-09-28 07:38:41 +0000

    nvim treesitter and nvim tree coincidental naming

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,6 +21,71 @@ return require("packer").startup(function(use)
   -- extended editor functionality
   use({ "mhinz/vim-grepper", cmd = "GrepperRg" })
 
+  use({
+    "kyazdani42/nvim-tree.lua",
+    requires = {
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        disable_netrw = false,
+        git = {
+          enable = true,
+        },
+        renderer = {
+          highlight_git = true,
+          icons = {
+            show = {
+              git = true,
+            },
+          },
+        },
+        view = {
+          side = "right",
+        },
+      })
+    end,
+  })
+
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    event = "BufRead",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = { "python", "javascript", "lua" },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        auto_install = true,
+
+        highlight = {
+          -- `false` will disable the whole extension
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        autotag = { -- Auto tag html
+          enable = true,
+          filetypes = { "html", "xml", "javascriptreact", "typescriptreact" },
+        },
+        rainbow = {
+          enable = false,
+          -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+          extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+          max_file_lines = nil, -- colors = {}, -- table of hex stringsnes = nil, -- Do not enable for files with more than n lines, int
+          -- termcolors = {} -- table of colour name strings
+        },
+      })
+    end,
+  })
+
   -- telescope
   use({
     "nvim-telescope/telescope.nvim",
