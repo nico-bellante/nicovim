0d15e299316b2fd0aa979876815ac80ae315a472
Nico Bellante <nico.bellante@discordapp.com>
2022-09-28 07:37:28 +0000

    adding keybindings

---
+++ lua/keybindings.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+local map = vim.api.nvim_set_keymap
+
+-- Moving Between Buffers
+map("n", "<C-h>", "<C-w>h", { noremap = true, silent = false })
+map("n", "<C-l>", "<C-w>l", { noremap = true, silent = false })
+map("n", "<C-j>", "<C-w>j", { noremap = true, silent = false })
+map("n", "<C-k>", "<C-w>k", { noremap = true, silent = false })
+
+map("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
